768cdfccecd4cab5c7c64ef4bd77cc5840ad3b4e
Patricio Pérez Pinto <patodeath@gmail.com>
2026-02-01 01:53:03 -0300

    refactor: replace ListDebtorsResult class with record for simplicity

--- src/main/java/cl/prezdev/balancehub/application/usecases/debtor/list/ListDebtorsResult.java
+++ src/main/java/cl/prezdev/balancehub/application/usecases/debtor/list/ListDebtorsResult.java
@@ -2,19 +2,6 @@ package cl.prezdev.balancehub.application.usecases.debtor.list;
 
 import java.util.List;
 
-public class ListDebtorsResult {
-
-    private List<DebtorListItem> debtors;
-
-    public ListDebtorsResult(List<DebtorListItem> debtors) {
-        this.debtors = debtors;
-    }
-
-    public List<DebtorListItem> getDebtors() {
-        return debtors;
-    }
-
-    public void setDebtors(List<DebtorListItem> debtors) {
-        this.debtors = debtors;
-    }
-}
+public record ListDebtorsResult (
+    List<DebtorListItem> debtors
+) {}
